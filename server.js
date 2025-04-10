8ec5a16b265f3b55708cfac7e7dea67d80dfb2a3
py660 <93660666+py660@users.noreply.github.com>
2025-04-10 15:01:36 -0400

    use other thingy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Listen on a specific host via the HOST environment variable
+/*// Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
@@ -47,3 +47,26 @@ cors_proxy.createServer({
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+*/
+
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, { target: 'https://www.ncps-k12.org' });
+});
+
+console.log("listening on port 5050")
+server.listen(5050);
